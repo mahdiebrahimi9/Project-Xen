2f4de9cf8d6548e000b472af1cd2d201fe99e950
Mahdi Ebrahimi <mahdi78eb@gmail.com>
2024-12-29 23:29:33 +0330

    # Get Component

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,8 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        GetComponent<MeshRenderer>().material.color = Color.yellow;
+            
         Debug.Log("Opss Hit Me!");
     }
 }
